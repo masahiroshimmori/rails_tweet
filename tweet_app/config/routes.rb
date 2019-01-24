2b50549e31fd643e67edff6fc6d25a8113489e8e
masahiro shimmori <shinmasa0412@yahoo.co.jp>
2019-01-24 23:14:36 +0900

    add new post

--- tweet_app/config/routes.rb
+++ tweet_app/config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get "/" => "home#top"
   get "about" => "home#about"
   get 'posts/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
+  post "posts/create" => "posts#create"
 end
