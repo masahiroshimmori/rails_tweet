c42de67edc850b1625ec918e726031ab33f3c590
masahiro shimmori <shinmasa0412@yahoo.co.jp>
2019-01-31 23:25:58 +0900

    add users_to_image_name

--- tweet_app/db/schema.rb
+++ tweet_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_29_134426) do
+ActiveRecord::Schema.define(version: 2019_01_31_132315) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_01_29_134426) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
   end
 
 end
